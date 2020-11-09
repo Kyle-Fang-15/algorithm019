f07d2d786f806ddb5a30a23b0cab43e181532271
ucan <Kyle>
2020-11-08 21:09:49 -0500

    94

---
+++ Week_02/94.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> memo = new ArrayList<Integer>();
+        traverse(root, memo);
+        return memo;
+        
+    }
+    private void traverse(TreeNode node, List<Integer> list){
+        if (node == null) return;
+        traverse(node.left, list);
+        list.add(node.val);
+        traverse(node.right, list);
+    }
+}
